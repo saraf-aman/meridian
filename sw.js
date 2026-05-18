cb1802a5196d58bc098ba89d54ae54da0a93de2c
Aman Saraf <amansaraf28@gmail.com>
2026-05-17 20:36:37 -0400

    nutrition - part 2

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-27c94f49';
+const CACHE = 'app-c09c70e5';
 
 // self.registration.scope resolves to the correct base regardless of
 // whether the site is deployed at the root or a sub-path (e.g. /meridian/).
