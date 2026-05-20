9ef55fa9cf1e4a459d6c6c3c4d378dc3abd0de48
Aman Saraf <amansaraf28@gmail.com>
2026-05-19 21:42:30 -0400

    Re-arrange Monday Exercise!

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'app-7127e9aa';
+const CACHE = 'app-39bf1b27';
 
 // self.registration.scope resolves to the correct base regardless of
 // whether the site is deployed at the root or a sub-path (e.g. /meridian/).
